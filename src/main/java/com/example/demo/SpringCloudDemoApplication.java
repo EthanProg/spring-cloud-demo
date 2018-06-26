121d8c941b723b657f565f67dcf1460022c6b92f
ethan <programcccethan@gmail.com>
2018-06-26 17:10:02 +0800

    spring cloud eureka server and client

--- src/main/java/com/example/demo/SpringCloudDemoApplication.java
+++ src/main/java/com/example/demo/SpringCloudDemoApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+// 启动服务注册中心
+//@EnableEurekaServer
+// 自动化配置
+@EnableDiscoveryClient
 public class SpringCloudDemoApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/demo/business/TestController.java
+++ src/main/java/com/example/demo/business/TestController.java
@@ -1,6 +1,11 @@
 package com.example.demo.business;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,6 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
+    private final Logger logger = Logger.getLogger(getClass());
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index(){
+        ServiceInstance instance = client.getLocalServiceInstance();
+        System.out.println("/hell0, host: " + instance.getHost() + ", service_id: " + instance.getServiceId());
+        return "Hello Wolrd";
+    }
 
     @RequestMapping("/test")
     public String test(){
