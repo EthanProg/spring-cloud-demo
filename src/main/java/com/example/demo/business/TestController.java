08673e591d4021bbe1d4e353f0f1f688bf262fee
ethan <programcccethan@gmail.com>
2018-06-26 14:59:07 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/business/TestController.java
@@ -0,0 +1,16 @@
+package com.example.demo.business;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liwenqing on 2018/6/26.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "Spring Boot";
+    }
+}
